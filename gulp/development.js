275e66c44e755c044bf65d5e2927a69fd0e37e38
danieljoppi <daniel.joppi@gmail.com>
2015-10-01 19:55:46 -0300

    - use strict in gulp

--- gulp/development.js
+++ gulp/development.js
@@ -1,46 +1,44 @@
-(function() {
-    'use strict';
+'use strict';
 
-    var gulp = require('gulp'),
-        gulpLoadPlugins = require('gulp-load-plugins'),
-        through = require('through'),
-        plugins = gulpLoadPlugins(),
-        appRoot = process.cwd(),
-        paths = {
-            js: [
-                appRoot + '/nsapi.js',
-                appRoot + '/src/**/*.js'
-            ],
-            jsTests: [appRoot + '/test/**/*-test.js']
-        };
+var gulp = require('gulp'),
+    gulpLoadPlugins = require('gulp-load-plugins'),
+    through = require('through'),
+    plugins = gulpLoadPlugins(),
+    appRoot = process.cwd(),
+    paths = {
+        js: [
+            appRoot + '/nsapi.js',
+            appRoot + '/src/**/*.js'
+        ],
+        jsTests: [appRoot + '/test/**/*-test.js']
+    };
 
-    var defaultTasks = ['env:development', 'dev:jshint', 'dev:coverage', 'watch'];
+var defaultTasks = ['env:development', 'dev:jshint', 'dev:coverage', 'watch'];
 
-    gulp.task('env:development', function () {
-        process.env.NODE_ENV = 'development';
-    });
+gulp.task('env:development', function () {
+    process.env.NODE_ENV = 'development';
+});
 
-    gulp.task('dev:jshint', function () {
-        return gulp.src(paths.js.concat(paths.jsTests))
-            .pipe(plugins.plumber())
-            .pipe(plugins.jshint())
-            .pipe(plugins.jshint.reporter('jshint-stylish'))
-            .pipe(plugins.jshint.reporter('fail'));
-    });
+gulp.task('dev:jshint', function () {
+    return gulp.src(paths.js.concat(paths.jsTests))
+        .pipe(plugins.plumber())
+        .pipe(plugins.jshint())
+        .pipe(plugins.jshint.reporter('jshint-stylish'))
+        .pipe(plugins.jshint.reporter('fail'));
+});
 
 
-    gulp.task('dev:coverage', function () {
-        return gulp.src(paths.jsTests)
-            .pipe(plugins.plumber())
-            .pipe(plugins.mocha({
-                reporters: 'spec'
-            }));
-    });
+gulp.task('dev:coverage', function () {
+    return gulp.src(paths.jsTests)
+        .pipe(plugins.plumber())
+        .pipe(plugins.mocha({
+            reporters: 'spec'
+        }));
+});
 
-    gulp.task('watch', function () {
-        gulp.watch(paths.js.concat(paths.jsTests), ['dev:jshint', 'dev:coverage'])
-            .on('error', e => console.error(e));
-    });
+gulp.task('watch', function () {
+    gulp.watch(paths.js.concat(paths.jsTests), ['dev:jshint', 'dev:coverage'])
+        .on('error', e => console.error(e));
+});
 
-    gulp.task('development', defaultTasks);
-})();
+gulp.task('development', defaultTasks);
--- gulp/test.js
+++ gulp/test.js
@@ -1,80 +1,78 @@
-(function() {
-    'use strict';
+'use strict';
 
-    var file, msg = '**/*-test.js';
-    process.argv.forEach(function (val, index, array) {
-        if (val === '-file' || val === '--f') {
-            let env_val = array[index + 1];
-            msg = '**/*' + env_val+ '*-test.js';
-            file = env_val;
-        }
-    });
-    console.log('use => load tests: ', msg);
+var file, msg = '**/*-test.js';
+process.argv.forEach(function (val, index, array) {
+    if (val === '-file' || val === '--f') {
+        let env_val = array[index + 1];
+        msg = '**/*' + env_val+ '*-test.js';
+        file = env_val;
+    }
+});
+console.log('use => load tests: ', msg);
 
-    var gulp = require('gulp'),
-        gulpLoadPlugins = require('gulp-load-plugins'),
-        plugins = gulpLoadPlugins(),
-        appRoot = process.cwd(),
-        paths = {
-            js: [
-                appRoot + '/nsapi.js',
-                appRoot + '/lib/**/*.js',
-                appRoot + '/src/**/*.js'
-            ],
-            jsRequire: [
-                appRoot + '/nsapi.js',
-                appRoot + '/src/**/*.js'
-            ],
-            jsVMContext: [appRoot + '/lib/**/*.js']
-        };
-    var defaultTasks = ['env:test', 'test:jshint', 'test:coverage'];
+var gulp = require('gulp'),
+    gulpLoadPlugins = require('gulp-load-plugins'),
+    plugins = gulpLoadPlugins(),
+    appRoot = process.cwd(),
+    paths = {
+        js: [
+            appRoot + '/nsapi.js',
+            appRoot + '/lib/**/*.js',
+            appRoot + '/src/**/*.js'
+        ],
+        jsRequire: [
+            appRoot + '/nsapi.js',
+            appRoot + '/src/**/*.js'
+        ],
+        jsVMContext: [appRoot + '/lib/**/*.js']
+    };
+var defaultTasks = ['env:test', 'test:jshint', 'test:coverage'];
 
-    gulp.task('env:test', function () {
-        process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-        process.env.NODE_ENV = 'test';
-    });
+gulp.task('env:test', function () {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+    process.env.NODE_ENV = 'test';
+});
 
-    gulp.task('test:jshint', function () {
-        return gulp.src(paths.js)
-            .pipe(plugins.jshint())
-            .pipe(plugins.jshint.reporter('jshint-stylish'))
-            .pipe(plugins.jshint.reporter('fail'));
-    });
+gulp.task('test:jshint', function () {
+    return gulp.src(paths.js)
+        .pipe(plugins.jshint())
+        .pipe(plugins.jshint.reporter('jshint-stylish'))
+        .pipe(plugins.jshint.reporter('fail'));
+});
 
-    gulp.task('test:coverage', function () {
-        let executeTests = function () {
-            let path = '/test/**/*' + (file ? file + '*' : '') + '-test.js';
-            gulp.src([appRoot + path])
-                .pipe(plugins.mocha({
-                    reporters: 'spec'
-                }))
-                .pipe(plugins.istanbul.writeReports({
-                    reports: ['lcovonly']
-                })); // Creating the reports after tests runned
-        };
+gulp.task('test:coverage', function () {
+    let executeTests = function () {
+        let path = '/test/**/*' + (file ? file + '*' : '') + '-test.js';
+        gulp.src([appRoot + path])
+            .pipe(plugins.mocha({
+                reporters: 'spec'
+            }))
+            .pipe(plugins.istanbul.writeReports({
+                reports: ['lcovonly']
+            })); // Creating the reports after tests runned
+    };
 
-        let actual = 0, verifyInstrument = () => { (++actual === 2) && executeTests(); };
+    let actual = 0, verifyInstrument = () => { (++actual === 2) && executeTests(); };
 
-        // instrumentation nsapi.js
-        gulp.src(paths.jsRequire)
-            .pipe(plugins.istanbul({
-                includeUntested: true,
-                instrumenter: require('isparta').Instrumenter
+    // instrumentation nsapi.js
+    gulp.src(paths.jsRequire)
+        .pipe(plugins.istanbul({
+            includeUntested: true,
+            instrumenter: require('isparta').Instrumenter
 
-            })) // Covering files
-            .pipe(plugins.istanbul.hookRequire())// Force `require` to return covered files
-            .on('finish', () => verifyInstrument());
+        })) // Covering files
+        .pipe(plugins.istanbul.hookRequire())// Force `require` to return covered files
+        .on('finish', () => verifyInstrument());
 
-        // instrumentation other js
-        gulp.src(paths.jsVMContext)
-            .pipe(plugins.istanbul({
-                includeUntested: true,
-                instrumenter: require('isparta').Instrumenter
+    // instrumentation other js
+    gulp.src(paths.jsVMContext)
+        .pipe(plugins.istanbul({
+            includeUntested: true,
+            instrumenter: require('isparta').Instrumenter
 
-            })) // Covering files
-            .pipe(plugins.istanbul.hookRunInThisContext()) // Force `vm.runInThisContext` to return covered files
-            .on('finish', () => verifyInstrument());
-    });
+        })) // Covering files
+        .pipe(plugins.istanbul.hookRunInThisContext()) // Force `vm.runInThisContext` to return covered files
+        .on('finish', () => verifyInstrument());
+});
 
-    gulp.task('test', defaultTasks);
-})();
+gulp.task('test', defaultTasks);
