f53dbf857c302e0a13ad189e2b43b8a47bb7ca80
danieljoppi <daniel.joppi@gmail.com>
2015-10-07 16:52:10 -0300

    - rename describe on schedule test

--- test/create-schedule-test.js
+++ test/create-schedule-test.js
@@ -7,7 +7,7 @@ var base = __dirname + '/_input-files/record-data';
 /**
  * Test Suites
  */
-describe('<Unit Test - Netsuite Create Suitelet>', function () {
+describe('<Unit Test - Netsuite Create Schedule>', function () {
     this.timeout(5000);
     before(function (done) {
         let metadatas = [
