15a32d5726f7169ef440951ae1223861445e0555
danieljoppi <daniel.joppi@gmail.com>
2015-10-20 10:53:56 -0200

    - XML API: implement tests to convert XML

---
+++ test/xml-api-test/string-to-xml-test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var fs = require('fs'),
+    path = require('path'),
+    should = require('should'),
+    nsmockup = require('../../');
+
+const fileDir = __dirname + '/../_input-files/files';
+
+/**
+ * Test Suites
+ */
+describe('<Unit Test - Netsuite XML API>', function () {
+
+    before(function (done) {
+        nsmockup.init(done);
+    });
+    describe('XML API - nlapiStringToXML:', function () {
+        it('xml convert string to xml document', function (done) {
+            let xmlPath = path.resolve(fileDir + '/help.xml'),
+                xml = fs.readFileSync(xmlPath, 'utf8'),
+                xmlDoc = nlapiStringToXML(xml);
+
+            should(xmlDoc).be.ok();
+            should(xmlDoc).have.property('firstChild');
+            should(xmlDoc).have.property('lastChild');
+            should(xmlDoc).have.property('nodeName');
+            should(xmlDoc).have.property('childNodes').length(1);
+            return done();
+        });
+
+        it('xml missing str', function (done) {
+            let xml = nlapiStringToXML();
+            should(xml).not.be.ok();
+            return done();
+        });
+    });
+    after(function (done) {
+        nsmockup.destroy(done);
+    });
+});
