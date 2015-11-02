6a76da15124865fda9b0343ccd9e8bf1ced8ee54
danieljoppi <daniel.joppi@gmail.com>
2015-11-02 21:01:58 -0200

    - Date API - add test 'no format' for nlapiStringToDate

--- test/date-api/string-date-test.js
+++ test/date-api/string-date-test.js
@@ -19,6 +19,22 @@ describe('<Unit Test - Netsuite Date API>', function () {
         nsmockup.init(opts, done);
     });
     parallel('SuiteScript API - nlapiStringToDate:', function () {
+        it('date no format', function (done) {
+            let now = new Date(),
+                str = now.toJSON(),
+                date = nlapiStringToDate(str);
+            should(date).have.instanceOf(Date);
+            should(date.getUTCDate()).be.equal(now.getUTCDate(), `Wrong day ${str} -> ${date.toJSON()}`);
+            should(date.getUTCMonth()).be.equal(now.getUTCMonth(), `Wrong month ${str} -> ${date.toJSON()}`);
+            should(date.getUTCFullYear()).be.equal(now.getUTCFullYear(), `Wrong year ${str} -> ${date.toJSON()}`);
+            should(date.getUTCHours()).be.equal(now.getUTCHours(), `Wrong hours ${str} -> ${date.toJSON()}`);
+            should(date.getUTCMinutes()).be.equal(now.getUTCMinutes(), `Wrong minutes ${str} -> ${date.toJSON()}`);
+            should(date.getUTCSeconds()).be.equal(now.getUTCSeconds(), `Wrong seconds ${str} -> ${date.toJSON()}`);
+            should(date.toJSON()).be.equal(str);
+
+            return done();
+        });
+
         it('date format "date"', function (done) {
             let str = '15-09-2015 08:58:12 -03:00',
                 date = nlapiStringToDate(str, 'date');
